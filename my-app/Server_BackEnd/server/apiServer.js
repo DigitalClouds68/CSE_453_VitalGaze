8943afdd3248655839e9fd3e1892305cff186fe1
DigitalClouds68 <DigitalClouds68@gmail.com>
2025-02-21 20:14:58 -0500

    Move .env from Server_BackEnd/ to my-app/

--- my-app/Server_BackEnd/server/apiServer.js
+++ my-app/Server_BackEnd/server/apiServer.js
@@ -12,5 +12,9 @@ app.use(express.json());
 // Load the API routes
 app.use("/api/auth", require("./routes/authRoutes"));
 
+/* ***********************后期补充************************👇*/
+//app.use("/api/sessions", require("./routes/sessionRoutes")); // 训练数据 API
+//app.use("/api/tracking", require("./routes/trackingRoutes")); // 眼动追踪 API
+
 const PORT = process.env.API_PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 API server is running at port ${PORT}`));
