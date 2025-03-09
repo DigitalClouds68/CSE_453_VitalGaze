5b40218c7b5228074f21b29f98ca88613160f6bf
YunzeZheng <53240464+YunzeZheng@users.noreply.github.com>
2025-03-09 12:23:18 -0400

    up_2

--- Xiao_ESP32/Wearable_glasses/src/leds_strip.cpp
+++ Xiao_ESP32/Wearable_glasses/src/leds_strip.cpp
@@ -1,20 +1,20 @@
 #include <Arduino.h>
-// #include <FastLED.h>
-// #include "main.h"
+#include <FastLED.h>
+#include "main.h"
 
-// #define LED_PIN 6
-// #define NUM_LEDS 16
+#define LED_PIN 6
+#define NUM_LEDS 16
 
-// CRGB leds[NUM_LEDS];
+CRGB leds[NUM_LEDS];
 
-// void setup() {
-//     FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
-// }
+void LED_setup() {
+    FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
+}
 
-// void loop() {
-//     for (int i = 0; i < NUM_LEDS; i++) {
-//         leds[i] = CRGB::Red; // Set to red
-//         FastLED.show();      // Update the strip
-//         delay(100);
-//     }
-// }
+void signal() {
+    for (int i = 0; i < NUM_LEDS; i++) {
+        leds[i] = CRGB::Red; // Set to red
+        FastLED.show();      // Update the strip
+        delay(100);
+    }
+}
--- Xiao_ESP32/Wearable_glasses/src/main.h
+++ Xiao_ESP32/Wearable_glasses/src/main.h
@@ -5,6 +5,6 @@
 
 // Function prototypes
 void AIcam_setup();  // Function to initialize the AI camera
-void AI_Dection();   // Function to perform AI detection
+void AI_Detection();   // Function to perform AI detection
 
 #endif // MAIN_H
