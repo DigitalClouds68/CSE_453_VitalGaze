7440eed744f53c12833708172c9f70a6414d3282
DigitalClouds68 <DigitalClouds68@gmail.com>
2025-03-09 16:19:21 -0400

    Update some scripts and sth(still cant work for WEBGL(自由切换横竖屏))

--- my-app/app/(tabs)/UnityWebGL.tsx
+++ my-app/app/(tabs)/UnityWebGL.tsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { View, ActivityIndicator } from "react-native";
 import { WebView } from "react-native-webview";
+import * as ScreenOrientation from "expo-screen-orientation";
 
 // 读取环境变量中的 WebGL 服务器地址
 const WEBGL_URL = process.env.EXPO_PUBLIC_WEBGL_URL || "http://localhost:8080/index.html";
 
 const UnityWebGLScreen = () => {
+  useEffect(() => {
+    // 进入 WebGL 页面时切换横屏
+    const lockOrientation = async () => {
+      await ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE);
+    };
+    lockOrientation();
+
+    // 退出 WebGL 页面时恢复竖屏
+    return () => {
+      ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.PORTRAIT_UP);
+    };
+  }, []);
+
   return (
     <View style={{ flex: 1 }}>
       <WebView
@@ -13,6 +27,7 @@ const UnityWebGLScreen = () => {
         style={{ flex: 1 }}
         startInLoadingState
         renderLoading={() => <ActivityIndicator size="large" color="#0000ff" />}
+        allowsFullscreenVideo // 确保 WebGL 支持全屏模式
       />
     </View>
   );
